c620b19548161d66a9c78daf09b42132874f9ff6
Benjamin Benitez <benjamin_benitez@outlook.com>
2024-08-31 22:13:14 -0300

    seccion prestamos y mediaquerys

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,21 @@
 const icon_list = document.getElementById("icon_list");
 const sidebar = document.querySelector(".sidebar");
 const spans= document.querySelectorAll("span");
+const menu = document.querySelector(".menu");
+
+
+menu.addEventListener("click",()=>{
+    sidebar.classList.toggle("max-sidebar");
+    if(sidebar.classList.contains("max-sidebar")){
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block";
+    }
+    else{
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";
+
+    }
+});
 
 
 icon_list.addEventListener("click",()=>{
